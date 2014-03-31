cfb48f9e73e11d83d1d0f3db82341c39dc4ab3f3
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2014-03-31 22:37:28 +0800

    More emacsy stuff.

---
+++ elpa/god-mode-2.12.0/god-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "god-mode" "2.12.0" "God-like command entering minor mode" (quote nil))
--- init.el
+++ init.el
@@ -14,5 +14,39 @@
 (evil-mode t)
 
 (setq cider-popup-stacktraces nil)
+(setq cider-repl-popup-stacktraces t)
+(setq cider-auto-select-error-buffer t)
+(setq cider-repl-wrap-history t)
+(require 'icomplete)
+(add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
 
 (load-theme 'solarized-dark t)
+
+(projectile-global-mode)
+
+(require 'auto-complete)
+(global-auto-complete-mode)
+
+(require 'ac-nrepl)
+(add-hook 'cider-repl-mode-hook 'ac-nrepl-setup)
+(add-hook 'cider-mode-hook 'ac-nrepl-setup)
+(eval-after-load "auto-complete"
+  '(add-to-list 'ac-modes 'cider-repl-mode))
+(eval-after-load "cider"
+  '(define-key cider-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc))
+
+; (require 'powerline)
+; (powerline-center-evil-theme)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#042028" :foreground "#708183" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "apple" :family "Monaco")))))
